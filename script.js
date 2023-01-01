82130db53cab5fb4c665ed59d668efc4e1275c5c
Evan Wauchope <evanwauchope@live.ca>
2023-01-01 16:40:45 -0500

    Added img buttons
    added img buttons linking to github & linkedin

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+/**
+ * @param {HTMLElement} hElement
+ */
+var onHover = function(hElement) {
+    hElement.style.opacity=.6;
+};
+/**
+ * @param {HTMLElement} hElement
+ */
+var onRmv = function(hElement){
+    hElement.style.opacity=1;
+    
+}
